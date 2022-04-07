24fe5ec23a5f11d1575262aba6454fb2b3138711
jyp17 <jypark1024@gmail.com>
2022-04-07 15:43:30 -0700

    Implement count button

--- src/application/FXMLController.java
+++ src/application/FXMLController.java
@@ -2,20 +2,29 @@ package application;
 
 import java.net.*;
 import java.util.*;
+import javafx.event.*;
 import javafx.fxml.*;
 import javafx.scene.control.*;
 
 public class FXMLController implements Initializable {
+    private static int count = 0;
+
     @FXML
     Button myButton;
 
     @FXML
     Label myLabel;
 
+    @FXML
+    protected void incrementCountButtonAction(ActionEvent event) {
+        count++;
+        myLabel.setText(String.valueOf(count));
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        myButton.setText("I am a button.");
-        myLabel.setText("Howdy");
+        myButton.setText("Count");
+        myLabel.setText("0");
     }
 
 }
